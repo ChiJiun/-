1f0199692dd6d8a42ac35c94fa931e1467322bff
Chi-Jiun <0311gino@gmail.com>
2023-11-04 14:33:22 +0800

    update2

--- Bar-chart/data.js
+++ Bar-chart/data.js
@@ -1,28 +1,127 @@
-let animals_Taipei_Zoo = [
+let not_pay_attention = [
     {
-        "name":"Lion",
-        "count":20
+        "name": "中壢",
+        "count": 1677
+    },    
+    {
+        "name": "桃園",
+        "count": 1243
     },
     {
-        "name":"Tiger",
-        "count":14
+        "name": "平鎮",
+        "count": 886
     },
     {
-        "name":"Monkey",
-        "count":23
-    }
+        "name": "大園",
+        "count": 795
+    },
+    {
+        "name": "龜山",
+        "count": 715
+    },
+    {
+        "name": "楊梅",
+        "count": 557
+    },
+    {
+        "name": "蘆竹",
+        "count": 513
+    },
+    {
+        "name": "龍潭",
+        "count": 308
+    },
+    {
+        "name": "大溪",
+        "count": 191
+    },
+    {
+        "name": "八德",
+        "count": 271
+    },
 ];
-let animals_Taoyuan_Zoo = [
+let violate_rules = [
     {
-        "name":"Lion",
-        "count":12
+        "name": "中壢",
+        "count": 982
     },
     {
-        "name":"Tiger",
-        "count":18
+        "name": "桃園",
+        "count": 830
     },
     {
-        "name":"Monkey",
-        "count":29
-    }
+        "name": "平鎮",
+        "count": 393
+    },
+    {
+        "name": "大園",
+        "count": 267
+    },
+    {
+        "name": "龜山",
+        "count": 239
+    },
+    {
+        "name": "楊梅",
+        "count": 450
+    },
+    {
+        "name": "蘆竹",
+        "count": 283
+    },
+    
+    {
+        "name": "龍潭",
+        "count": 289
+    },
+    {
+        "name": "大溪",
+        "count": 113
+    },
+    {
+        "name": "八德",
+        "count": 182
+    },
 ];
+let turn_left = [
+    {
+        "name": "中壢",
+        "count": 392
+    },   
+    {
+        "name": "桃園",
+        "count": 340
+    },
+    {
+        "name": "平鎮",
+        "count": 310
+    },
+    {
+        "name": "大園",
+        "count": 86
+    },
+    {
+        "name": "龜山",
+        "count": 162
+    },
+    {
+        "name": "楊梅",
+        "count": 130
+    },
+    {
+        "name": "蘆竹",
+        "count": 126
+    },
+    {
+        "name": "龍潭",
+        "count": 192
+    },
+    {
+        "name": "大溪",
+        "count": 69
+    },
+    {
+        "name": "八德",
+        "count": 89
+    }
+]
--- Bar-chart/main.js
+++ Bar-chart/main.js
@@ -1,82 +1,40 @@
-let myGraph2 = document.getElementById("myGraph2");
-let HW2_trace1 = {};
-HW2_trace1.type = "bar";
-HW2_trace1.name = "Lion";
-HW2_trace1.x = [];
-HW2_trace1.y = [];
+// 取得每個地區的名稱
+let locations = not_pay_attention.map(item => item.name);
 
-HW2_trace1.x[0] = "Taipei Zoo";
-HW2_trace1.y[0] = animals_Taipei_Zoo[0]['count'];
-HW2_trace1.x[1] = "Taoyuan Zoo";
-HW2_trace1.y[1] = animals_Taoyuan_Zoo[0]['count'];
+// 取得每個類別的數據
+let notPayAttentionCounts = not_pay_attention.map(item => item.count);
+let violateRulesCounts = violate_rules.map(item => item.count);
+let turnLeftCounts = turn_left.map(item => item.count);
 
-HW2_trace1.text = HW2_trace1.y;
-HW2_trace1.textfont = {
-    color: 'white',
-    size:20
-};
+// 創建長條圖的資料
+let data2 = [{
+        x: locations,
+        y: notPayAttentionCounts,
+        name: '未注意車前狀況',
+        type: 'bar'
+    },
+    {
+        x: locations,
+        y: violateRulesCounts,
+        name: '未依規定讓車',
+        type: 'bar'
+    },
+    {
+        x: locations,
+        y: turnLeftCounts,
+        name: '未依規定左轉彎',
+        type: 'bar',
+    }
+];
 
-let HW2_trace2 = {};
-HW2_trace2.type = "bar";
-HW2_trace2.name = "Tiger";
-HW2_trace2.x = [];
-HW2_trace2.y = [];
-
-HW2_trace2.x[0] = "Taipei Zoo";
-HW2_trace2.y[0] = animals_Taipei_Zoo[1]['count'];
-HW2_trace2.x[1] = "Taoyuan Zoo";
-HW2_trace2.y[1] = animals_Taoyuan_Zoo[1]['count'];
-
-HW2_trace2.text = HW2_trace2.y;
-HW2_trace2.textfont = {
-    color: 'white',
-    size: 20
-};
-
-let HW2_trace3 = {};
-HW2_trace3.type = "bar";
-HW2_trace3.name = "Monkey";
-HW2_trace3.x = [];
-HW2_trace3.y = [];
-
-HW2_trace3.x[0] = "Taipei Zoo";
-HW2_trace3.y[0] = animals_Taipei_Zoo[2]['count'];
-HW2_trace3.x[1] = "Taoyuan Zoo";
-HW2_trace3.y[1] = animals_Taoyuan_Zoo[2]['count'];
-
-HW2_trace3.text = HW2_trace3.y;
-HW2_trace3.textfont = {
-    color: 'white',
-    size: 20
-};
-
-// for(let i=0;i<animals_Taipei_Zoo.length;i++){
-//     trace1.x[i] = animals_Taipei_Zoo[i]['name'];
-//     trace1.y[i] = animals_Taipei_Zoo[i]['count'];
-// }
-
-// let trace2 = {};
-// trace2.type = "bar";
-// trace2.name = "Taoyuan Zoo";
-// trace2.x = [];
-// trace2.y = [];
-
-// for (let i = 0; i < animals_Taoyuan_Zoo.length; i++) {
-//     trace2.x[i] = animals_Taoyuan_Zoo[i]['name'];
-//     trace2.y[i] = animals_Taoyuan_Zoo[i]['count'];
-// }
-
-let data2 = [];
-data2.push(HW2_trace1);
-data2.push(HW2_trace2);
-data2.push(HW2_trace3);
-
+// 設定圖表佈局
 let layout2 = {
     margin: {
-        t: 50
-    },
-    barmode:'stack',
-    title:'Taipei Zoo VS. Taoyuan Zoo'
+    t: 50
+},
+    title: '桃園各地區三大交通事故原因',
+    barmode: 'stack'
 };
 
+// 在指定的 <div> 中顯示圖表
 Plotly.newPlot(myGraph2, data2, layout2);
